2252c6b9dea4bc245423b02846c10a5273908ea6
Pavel Jetensky <pavel.jetensky@plus4u.net>
2022-09-26 15:48:58 +0200

    Presentation version of files

--- src/statements/conditionals-4.js
+++ src/statements/conditionals-4.js
@@ -8,10 +8,11 @@ class Order {
   process() {
     if (this.isSpecialDeal()) {
       this.total = this.price * 0.95;
+      this.send();
     } else {
       this.total = this.price * 0.98;
+      this.send();
     }
-    this.send();
   }
 
   isSpecialDeal() {
